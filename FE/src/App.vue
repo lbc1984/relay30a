ae60163944ec7b76b9ad621c53f10b078f98fab9
ly.cuong <ly.cuong@marketenterprise.vn>
2025-03-03 21:19:02 +0700

    commit

--- FE/src/App.vue
+++ FE/src/App.vue
@@ -1,13 +1,31 @@
 <template>
   <v-app>
-    <router-view :key="$route.fullPath"></router-view>
+    <router-view v-if="isAuthChecked" :key="$route.fullPath"></router-view>
   </v-app>
 </template>
 
 <script>
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { useRouter } from "vue-router";
+import { ref, onMounted } from "vue";
+
 export default {
   name: "App",
+  setup() {
+    const router = useRouter();
+    const isAuthChecked = ref(false);
+
+    onMounted(() => {
+      onAuthStateChanged(auth, (user) => {
+        isAuthChecked.value = true; // Đánh dấu đã kiểm tra Auth
+        if (user) {
+          router.push("/"); // Chuyển về trang chính nếu đã đăng nhập
+        }
+      });
+    });
+
+    return { isAuthChecked };
+  },
 };
 </script>
-
-<style src="@/assets/css/styles.css"></style>
--- FE/src/components/Login.vue
+++ FE/src/components/Login.vue
@@ -1,67 +1,31 @@
 <template>
-    <div class="login-container">
-      <button @click="loginWithGoogle">Đăng nhập</button>
-    </div>
-  </template>
-  
-  <style scoped>
-  .login-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh; /* Chiều cao toàn màn hình */
-    text-align: center;
-  }
-  
-  button {
-    padding: 10px 20px;
-    font-size: 16px;
-    cursor: pointer;
-  }
-  </style>
-  
+  <div class="login-container">
+    <button @click="loginWithGoogle">Đăng nhập</button>
+  </div>
+</template>
+
+<style src="@/assets/css/login.css"></style>
 
 <script>
-import { ref } from "vue";
 import { auth } from "../firebase";
-import {
-  signInWithEmailAndPassword,
-  signInWithPopup,
-  GoogleAuthProvider,
-  getAuth,
-} from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { useRouter } from "vue-router";
 
 export default {
   setup() {
-    const email = ref("");
-    const password = ref("");
     const router = useRouter();
-
-    const login = async () => {
-      try {
-        await signInWithEmailAndPassword(auth, email.value, password.value);
-        alert("Đăng nhập thành công!");
-
-        const auth = getAuth();
-        auth.currentUser.getIdToken(true).then((idToken) => {
-          alert(idToken);
-        });
-      } catch (error) {
-        alert(error.message);
-      }
-    };
 
     const loginWithGoogle = async () => {
       try {
         const provider = new GoogleAuthProvider();
         const result = await signInWithPopup(auth, provider);
-
         const user = result.user;
         const idToken = await user.getIdToken(true);
 
         localStorage.setItem("google_access_token", idToken);
+
+        await new Promise((resolve) => setTimeout(resolve, 100));
+
         router.push("/");
       } catch (error) {
         console.error("Lỗi đăng nhập:", error.code, error.message);
@@ -69,7 +33,7 @@ export default {
       }
     };
 
-    return { email, password, login, loginWithGoogle };
+    return {loginWithGoogle };
   },
 };
 </script>
